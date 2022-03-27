9b8d53cde6380adcf25c2eaa1956e1a419c17e79
vinoya21 <45473119+vinoya21@users.noreply.github.com>
2022-03-27 15:17:05 -0700

    cleaned up folder

--- fast5 to fastq/poretools/poretools/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-from . import scripts
-from .Fast5File import *
-from .version import __version__
--- fast5 to fastq/poretools/poretools/scripts/poretools-script.py
+++
@@ -1,5 +0,0 @@
-#!C:\Anaconda\python.exe
-# EASY-INSTALL-SCRIPT: 'poretools==0.2.0','poretools'
-__requires__ = 'poretools==0.2.0'
-import pkg_resources
-pkg_resources.run_script('poretools==0.2.0', 'poretools')
